a744424009d00ba1f3e61e27432458cf96edb22c
romina2911 <romina.hp@gmail.com>
2017-11-20 11:17:53 -0500

    subiendo a repositorio remoto

---
+++ js/app.js
@@ -0,0 +1,22 @@
+//definiendo variables
+var list = document.getElementById('list');
+var work = document.getElementById('work');
+var sabe = document.getElementById('save');
+
+window.addEventListener ('load', function(){
+    
+    function show() {
+        list = document.getElementById('work');
+        list.style.display = '';
+    }
+
+    function close() {
+        list = document.getElementById('work');
+        dlist.style.display = 'none';
+    }
+    });
+
+
+
+
+});
